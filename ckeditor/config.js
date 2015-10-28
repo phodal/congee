2af09cd7d5aeb129ee2991e07a5467f91d8e3fa2
Fengda HUANG <h@phodal.com>
2015-10-28 21:33:28 +0800

    custom toolbar

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -16,14 +16,8 @@ CKEDITOR.editorConfig = function (config) {
 
 
   config.toolbar = [
-        {name: 'document', items: ['Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates']},
-        {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-        {name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt']},
-        {
-            name: 'forms', items: ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
-            'HiddenField']
-        },
-        '/',
+        {name: 'document', items: ['Preview', 'Templates']},
+        {name: 'clipboard', items: ['Undo', 'Redo']},
         {
             name: 'basicstyles',
             items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
@@ -31,16 +25,11 @@ CKEDITOR.editorConfig = function (config) {
         {
             name: 'paragraph',
             items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv',
-                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl']
-        },
-        {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
-        {
-            name: 'insert',
-            items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']
+                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
         },
         '/',
-        {name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize']},
+        {name: 'styles', items: ['Format', 'Font', 'FontSize']},
         {name: 'colors', items: ['TextColor', 'BGColor']},
-        {name: 'tools', items: ['Maximize', 'ShowBlocks', '-', 'About']}
+        {name: 'tools', items: ['Maximize', 'ShowBlocks', 'Source']}
     ];
 };
