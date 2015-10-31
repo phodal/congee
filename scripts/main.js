21f8cc256a9f8c268ef7b5d97e405f94d3f4d0b4
Fengda HUANG <h@phodal.com>
2015-10-31 14:27:17 +0800

    Add spectrum color picker & fixed #13

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,8 @@ require.config({
     ractive: 'scripts/libs/ractive.min',
     rv: 'scripts/libs/rv',
     amplify: 'scripts/libs/amplify.min',
-    'amplify.request': 'scripts/libs/amplify.request.min'
+    'amplify.request': 'scripts/libs/amplify.request.min',
+    'spectrum': 'scripts/spectrum/spectrum'
   },
   'shim': {
     ko: {
@@ -28,6 +29,9 @@ require.config({
       deps: ['jquery'],
       exports: 'amplify'
     },
+    'spectrum': {
+      deps: ['jquery']
+    },
     'jquery.mixitup': {
       deps: ['jquery']
     },
@@ -37,7 +41,7 @@ require.config({
   }
 });
 
-require([ 'ractive', 'scripts/init', 'scripts/config', 'amplify', 'amplify.request'], function (Ractive, init, config, amplify) {
+require([ 'ractive', 'scripts/init', 'scripts/config', 'amplify', 'amplify.request', 'spectrum'], function (Ractive, init, config, amplify) {
   'use strict';
   init();
   var appConfig = config;
@@ -63,14 +67,31 @@ require([ 'ractive', 'scripts/init', 'scripts/config', 'amplify', 'amplify.reque
 
   var colors = new Ractive({
     el: 'colors',
-    template: '<input placeholder="Type your name" value="{{globalColor}}">',
+    template: '<input placeholder="Type your name" id="colorpicker" value="{{globalColor}}">',
     data: {globalColor: globalColor}
   });
 
-  colors.observe( 'globalColor', function ( newValue ) {
+  colors.observe( '.sp-preview-inner', function ( newValue ) {
     console.log(newValue);
     appConfig.defaultColor = newValue;
     this.fire('changeGlobalColor');
   });
 
+  $("#colorpicker").spectrum({
+    showPaletteOnly: true,
+    togglePaletteOnly: true,
+    togglePaletteMoreText: 'more',
+    togglePaletteLessText: 'less',
+    color: 'blanchedalmond',
+    palette: [
+      ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+      ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+      ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
+      ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+      ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+      ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+      ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+      ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+    ]
+  });
 });
