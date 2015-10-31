05569d08dfca19210bf925a85d51a9b50f4a56f0
Fengda HUANG <h@phodal.com>
2015-10-31 23:13:12 +0800

    add dropdown arrow

--- scripts/spectrum/spectrum.js
+++ scripts/spectrum/spectrum.js
@@ -75,7 +75,7 @@
     replaceInput = [
         "<div class='sp-replacer'>",
             "<div class='sp-preview'><div class='sp-preview-inner'></div></div>",
-            "<div class='sp-dd'>&#9660;</div>",
+            "<div class='sp-dd'></div>",
         "</div>"
     ].join(''),
     markup = (function () {
