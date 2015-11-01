2ed1946f4dd4c470f4d3aab646d7938925c34682
Fengda HUANG <h@phodal.com>
2015-11-01 17:53:12 +0800

    Extarct color picker

--- scripts/main.js
+++ scripts/main.js
@@ -53,26 +53,8 @@ require(['scripts/init', 'scripts/views/titleView','scripts/views/hrView', 'spec
   var titleView = TitleView.init(config);
   var hrView = HRView.init(config);
 
-  $("#colorpicker").spectrum({
-    showPaletteOnly: true,
-    togglePaletteOnly: true,
-    togglePaletteMoreText: 'more',
-    togglePaletteLessText: 'less',
-    color: '#4caf50',
-    palette: [
-      ["#1abc9c","#16a085","#2ecc71","#27ae60","#4caf50","#8bc34a","#cddc39"],
-      ["#3498db","#2980b9","#34495e","#2c3e50","#2196f3","#03a9f4","#00bcd4","#009688"],
-      ["#e74c3c","#c0392b","#f44336"],
-      ["#e67e22","#d35400","#f39c12","#ff9800","#ff5722","#ffc107"],
-      ["#f1c40f","#ffeb3b"],
-      ["#9b59b6","#8e44ad","#9c27b0","#673ab7","#e91e63","#3f51b5"],
-      ["#795548"],
-      ["#9e9e9e","#607d8b","#7f8c8d","#95a5a6","#bdc3c7"],
-      ["#ecf0f1","efefef"]
-    ],
-    change: function(color) {
-      titleView.fire('changeColor', {color: color.toHexString()});
-      hrView.fire('changeColor', {color: color.toHexString()});
-    }
-  });
+  Init.colorPicker(function(color){
+    titleView.fire('changeColor', {color: color.toHexString()});
+    hrView.fire('changeColor', {color: color.toHexString()});
+  })
 });
