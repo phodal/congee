d050d3eebdf6e87b3bbf569897b1aa42a378d2a8
Fengda HUANG <h@phodal.com>
2015-11-03 22:57:40 +0800

    add support for qq music & fixed #16

--- ckeditor/plugins/music/dialogs/music.js
+++ ckeditor/plugins/music/dialogs/music.js
@@ -1,22 +1,123 @@
-CKEDITOR.dialog.add( 'music', function( editor ) {
-  return {
-    title: "Music",
-    minWidth: 270,
-    minHeight: 120,
-    contents: [ {
-      id: 'music',
-      label: '',
-      title: '',
-      expand: true,
-      padding: 0,
-      elements: [
-        {
-          type: 'html',
-          html: 'Hello, World'
-        }
-      ]
-    } ],
-    buttons: [ CKEDITOR.dialog.cancelButton, CKEDITOR.dialog.okButton ]
-  };
+function loadValue(musicNode) {
+  if (musicNode.hasAttribute(this.id)) {
+    var value = musicNode.getAttribute(this.id);
+    this.setValue(value);
+  }
+}
 
-} );
+function getJsonFromUrl(query) {
+  var result = {};
+  console.log(query);
+  if (query.toString().indexOf('&') === -1 || query.toString().indexOf('=') === -1) {
+    return {}
+  }
+  query.split("&").forEach(function (part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+function appendData(musicNode) {
+  var value = this.getValue();
+  var jsonData = getJsonFromUrl(value);
+  musicNode.setAttribute("mid", jsonData.mid);
+  musicNode.setAttribute("albumurl", "/N/Y/002Qp0Jv1PFKNY.jpg");
+  musicNode.setAttribute("audiourl", "http://ws.stream.qqmusic.qq.com/657177.m4a?fromtag=46");
+  musicNode.setAttribute("commentid", "1873023090");
+  musicNode.setAttribute("mid", "002lFkz43D4bvB");
+  musicNode.setAttribute("musicid", "657177");
+  musicNode.setAttribute("class", "res_iframe qqmusic_iframe js_editor_qqmusic");
+  musicNode.setAttribute("play_length", "236000");
+  musicNode.setAttribute("src", "https://mp.weixin.qq.com/cgi-bin/readtemplate?t=tmpl/qqmusic_tmpl&amp;singer=%E8%94%A1%E6%B7%B3%E4%BD%B3%20-%20%E5%9B%9E%E5%88%B0%E6%9C%80%E5%88%9D%EF%BC%88%E6%96%B0%E5%8A%A0%E5%9D%A1%E5%BA%86%E5%8A%9F%E7%89%88%EF%BC%89&amp;music_name=%E5%9B%9E%E5%88%B0%E6%9C%80%E5%88%9D");
+}
+
+function commitValue(musicNode) {
+  var value = this.getValue();
+  musicNode.setAttribute(this.att || this.id, value);
+}
+
+CKEDITOR.dialog.add('music', function (editor) {
+  return {
+    title: "QQ音乐",
+    minWidth: 480,
+    minHeight: 240,
+    onShow: function () {
+      this.fakeImage = this.musicNode = null;
+
+      var fakeImage = this.getSelectedElement();
+      if (fakeImage && fakeImage.data('cke-real-element-type') && fakeImage.data('cke-real-element-type') == 'iframe') {
+        this.fakeImage = fakeImage;
+
+        var musicNode = editor.restoreRealElement(fakeImage);
+        this.musicNode = musicNode;
+
+        this.setupContent(musicNode);
+      }
+    },
+    onOk: function () {
+      var musicNode;
+      if (!this.fakeImage)
+        musicNode = new CKEDITOR.dom.element('iframe');
+      else
+        musicNode = this.musicNode;
+
+      var extraStyles = {},
+        extraAttributes = {};
+      this.commitContent(musicNode, extraStyles, extraAttributes);
+
+      var newFakeImage = editor.createFakeElement(musicNode, 'cke_iframe', 'iframe', true);
+      newFakeImage.setAttributes(extraAttributes);
+      newFakeImage.setStyles(extraStyles);
+      if (this.fakeImage) {
+        newFakeImage.replace(this.fakeImage);
+        editor.getSelection().selectElement(newFakeImage);
+      } else {
+        editor.insertElement(newFakeImage);
+      }
+    },
+    contents: [{
+      id: 'music',
+      label: "QQ音乐",
+      elements: [{
+        type: 'html',
+        html: '<a href="http://y.qq.com/" target="_blank">打开QQ音乐</a>'
+      }, {
+        type: 'vbox',
+        padding: 0,
+        children: [{
+          id: 'music_url',
+          type: 'text',
+          label: "QQ音乐地址",
+          required: true,
+          validate: CKEDITOR.dialog.validate.notEmpty("QQ音乐地址: 不能为空"),
+          setup: loadValue,
+          commit: appendData
+        }]
+      },
+        {
+          type: 'hbox',
+          children: [{
+            id: 'singer',
+            type: 'text',
+            style: 'width:100%',
+            labelLayout: 'vertical',
+            label: "歌手",
+            validate: CKEDITOR.dialog.validate.notEmpty("歌手: 不能为空"),
+            setup: loadValue,
+            commit: commitValue
+          },
+            {
+              id: 'music_name',
+              type: 'text',
+              style: 'width:100%',
+              labelLayout: 'vertical',
+              label: "歌曲",
+              validate: CKEDITOR.dialog.validate.notEmpty("歌曲: 不能为空"),
+              setup: loadValue,
+              commit: commitValue
+            }]
+        }]
+    }]
+  };
+});
--- ckeditor/plugins/music/plugin.js
+++ ckeditor/plugins/music/plugin.js
@@ -4,15 +4,16 @@ CKEDITOR.plugins.add('music',
     icons: 'music',
     //hidpi: true,
     init: function (editor) {
-      editor.addCommand( 'music', new CKEDITOR.dialogCommand( 'music', {
-        allowedContent: 'iframe',
+      editor.addCommand('music', new CKEDITOR.dialogCommand('music', {
+        allowedContent: 'iframe[align,longdesc,frameborder,height,name,scrolling,src,title,width]',
         requiredContent: 'iframe'
-      } ) );
-      editor.ui.addButton && editor.ui.addButton( 'Music', {
+      }));
+      editor.ui.addButton && editor.ui.addButton('Music', {
         label: editor.lang.smiley.toolbar,
         command: 'music',
         toolbar: 'insert,50'
-      } );
-      CKEDITOR.dialog.add( 'music', this.path + 'dialogs/music.js' );
+      });
+
+      CKEDITOR.dialog.add('music', this.path + 'dialogs/music.js');
     }
   });
