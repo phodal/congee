eb308d7df5701744b7de32372e088e98ac1fcaec
Fengda HUANG <h@phodal.com>
2015-11-05 07:42:59 +0800

    change a little title

--- scripts/views/titleView.js
+++ scripts/views/titleView.js
@@ -1,20 +1,43 @@
-define(['ractive', 'text!views/titles/hello.html'], function (Ractive, listTemplate) {
+define(['ractive', 'text!views/titles/list.html'], function (Ractive, titleTemplate) {
   'use strict';
-  var listView = null;
+  'use strict';
+  var titleView = null;
 
   var app = {
     init: function (config) {
-      listView = new Ractive({
+      var Grid = Ractive.extend({
+        isolated: false,
+        template: titleTemplate,
+        data: {
+
+        }
+      });
+      //Styles Come from http://codepen.io/ibrahimjabbari/pen/ozinB
+      var dataValue = 5;
+      var category = 'category-1';
+
+      var color = config.defaultColor;
+
+      titleView = new Ractive({
         el: 'sandboxTitle',
-        template: listTemplate,
-        data: {color: config.defaultColor, 'fontSize': config.defaultFontSize}
+        template: '<Grid Style="{{styles}}" />',
+        components: {Grid: Grid},
+        data: {
+          styles: [
+            {section_style: 'margin:1em auto;white-space: normal;border-style: none;text-align: center;', color: color,  data_value: dataValue, category: category}
+          ]
+        }
       });
 
-      listView.on('changeColor', function (args) {
-        listView.set('color', args.color);
+      titleView.on('changeColor', function(args) {
+        titleView.findComponent('Grid').set('Style.*.color', args.color);
       });
 
-      return listView;
+      return titleView;
+    },
+
+    toStyle: function(data){
+
     }
   };
 
