e50f5f3fa774bcbce9adbaf3260f3c59ad4b4b7a
Fengda HUANG <h@phodal.com>
2015-11-05 09:10:33 +0800

    Add Select all to menu

--- ckeditor/plugins/toCopy/plugin.js
+++ ckeditor/plugins/toCopy/plugin.js
@@ -3,15 +3,21 @@ CKEDITOR.plugins.add("tocopy",
     require: 'notification',
     icons: 'tocopy',
     init: function (editor) {
-      editor.ui.addButton && editor.ui.addButton('tocopy', {
-        label: "复制全部",
-        command: 'copyall',
-        toolbar: 'insert,50'
+      editor.addCommand('tocopy', {
+        modes: {wysiwyg: 1, source: 1},
+        exec: function (editor) {
+          editor.document.$.execCommand( 'SelectAll', false, null );
+          setTimeout(function () {
+            editor.document.$.execCommand('Copy');
+          }, 0);
+        },
+        canUndo: false
       });
 
-      editor.execCommand('selectAll');
-      setTimeout(function () {
-        editor.execCommand('copy');
-      }, 0);
+      editor.ui.addButton && editor.ui.addButton('tocopy', {
+        label: "复制全部",
+        command: 'tocopy',
+        toolbar: 'insert,50'
+      });
     }
   });
